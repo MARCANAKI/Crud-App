34ad99a993407b6dee6031238e0554e415662843
MARCANAKI <augustmarc16@gmail.com>
2025-02-26 00:35:16 +0100

    undefined error

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,29 @@
-import { Text, View, TextInput, Pressable, StyleSheet, FlatList  } from "react-native";
+import { Text, View, TextInput, Pressable, StyleSheet, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
+import { Inter_500Medium, useFonts } from "@expo-google-fonts/inter";
+
+import { Octicons } from "@expo/vector-icons/Octicons";
 
 import {data} from "@/data/todos"
 
 export default function Index() {
   const [todos, setTodos] = useState(data.sort((a,b) => b.id - a.id))
   const [text, setText] = useState('')
+  const {colorScheme, setColorScheme, theme} = useContext(ThemeContext)
+
+  const [loaded, error] = useFonts({
+    Inter_500Medium,
+  })
+
+  if (!loaded && !error) {
+    return null
+  }
+
+  const styles = createStyles(theme, colorScheme)
 
   const addTodo = () => {
     if (text.trim()) {
@@ -27,8 +43,11 @@ export default function Index() {
 
   const renderItem=({ item }) => (
     <View style={styles.todoItem}>
-      <Text style= {[styles.todoText, item.completed && styles.completedText ]}>{item.title}</Text>
-      <Pressable>
+      <Text style= {[styles.todoText, item.completed && styles.completedText ]}
+      onPress={() => toggleTodo(item.id)}
+      >
+        {item.title}</Text>
+      <Pressable onPress={() => removeTodo(item.id)}>
       <MaterialCommunityIcons name="delete-circle" size={36} color="red" selectable= {undefined} />
       </Pressable>
     </View>
@@ -47,22 +66,28 @@ export default function Index() {
         <Pressable onPress={addTodo} style={styles.addButton}>
           <Text style={styles.addButtonText}>Add</Text> 
         </Pressable>
+        <Pressable
+          onPress={() => setColorScheme(colorScheme === 'light' ? 'dark' : 'light')} style = {{marginLeft: 10}}>
+           {colorScheme === 'dark' 
+    ? <Octicons name="moon" size={36} color={theme.text} selectable={undefined} style={{ width: 36 }} /> 
+    : <Octicons name="sun" size={36} color={theme.text} selectable={undefined} style={{ width: 36 }} />}
+        </Pressable>
       </View>
-      <Flatlist
+      <FlatList
         data={todos}
-        renderItem={}
-        keyEtractor={todo => todo.id }
+        renderItem={renderItem}
+        keyExtractor={todo => todo.id }
         contentContainerStyle={{ flexGrow: 1 }}
       />
 
       </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
+function createStyles(theme, colorScheme){
+  return StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: theme.background,
   },
   inputContainer: {
     flexDirection: 'row',
@@ -82,18 +107,44 @@ const styles = StyleSheet.create({
     padding: 10,
     marginRight: 10,
     fontSize: 18,
+    fontFamily: 'Inter_500Medium',
     minWidth: 0,
-    color: 'white',
+    color: theme.text,
   },
   addButton: {
-    backgroundColor: 'white',
+    backgroundColor: theme.button,
     borderRadius: 5,
     padding: 10,
   },
   addButtonText: {
     fontSize: 18,
-    color: 'black',
+    color: colorScheme === 'dark' ? 'black' : 'white',
+  },
+  todoItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    gap: 4,
+    padding: 10,
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
+    width: '100%',
+    maxWidth: 1024,
+    marginHorizontal: 'auto',
+    pointerEvents: 'auto',
+
+  },
+  todoText: {
+    flex: 1,
+    fontSize: 18,
+    fontFamily: 'Inter_500Medium',
+    color: theme.text,
+  },
+  completedText: {
+    textDecorationLine: 'line-through',
+    color: 'gray',
   }
 
 
-})
+  })
+}
